5b49d0a6ecbfdd05d5612b2b9e742ebe9c90c3d8
Sebastian Walter <sebastian.walter98@gmail.com>
2022-05-30 20:25:36 +0200

    update help script

--- tokenization_repair/help.sh
+++ tokenization_repair/help.sh
@@ -3,11 +3,11 @@
 echo "
 Welcome to the tokenization repair project!
 
-If you want to use the tokenization repair models (more details at https://github.com/bastiscode/trt#from-command-line):
-  trt -h
+If you want to use the tokenization repair models (more details at https://github.com/ad-freiburg/trt#from-command-line):
+  wr -h
 
-If you want to start a tokenization repair server (port 12345):
-  trt --server 0.0.0.0:12345
+If you want to start a tokenization repair server:
+  wr --server /trt/configs/server.json
 
 If you want to reproduce the results from the paper by training yourself:
   Read the README.md for instructions (e.g. use 'cat README.md' or open it in the browser or a markdown viewer)
