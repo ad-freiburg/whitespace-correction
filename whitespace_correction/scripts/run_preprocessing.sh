9c28afaa698702a83f199c402ad997d56a7bbac7
Sebastian Walter <sebastian.walter98@gmail.com>
2022-11-08 19:02:00 +0100

    update preprocessing slurm script

--- whitespace_correction/scripts/run_preprocessing.sh
+++ whitespace_correction/scripts/run_preprocessing.sh
@@ -1,26 +1,14 @@
 #!/bin/bash
 #SBATCH --partition=alldlc_gpu-rtx2080
 #SBATCH --nodes=1
-#SBATCH --mincpus=32
-#SBATCH --mem=256G
-#SBATCH --job-name=preprocessing_tokenization_repair
-#SBATCH --mail-user=sebastian.walter98@gmail.com
+#SBATCH --mincpus=8
+#SBATCH --mem=64G
+#SBATCH --job-name=preprocessing
+#SBATCH --mail-user=swalter@cs.uni-freiburg.de
 #SBATCH --mail-type=END,FAIL
 #SBATCH --time=12:00:00
 
-export WORKSPACE_DIR=/work/dlclarge1/swalter-tokenization_repair/tokenization_repair
+workspace=/work/dlclarge2/swalter-whitespace_correction
 
-export DATA_INPUT_DIR=$WORKSPACE_DIR/data/cleaned
-export DATA_OUTPUT_DIR=$TMPDIR/data/preprocessed
-
-export TOKENIZER_DIR=$WORKSPACE_DIR/tokenizers
-export MISSPELLINGS_DIR=$WORKSPACE_DIR/misspellings
-export DICTIONARIES_DIR=$WORKSPACE_DIR/dictionaries
-
-echo "workspace dir: $WORKSPACE_DIR"
-echo "data input dir: $DATA_INPUT_DIR"
-echo "data output dir: $DATA_OUTPUT_DIR"
-
-python -m trt.preprocess_data --config ${CONFIG?"CONFIG env variable not found"}
-
-rsync -r -ah --progress $DATA_OUTPUT_DIR/ $WORKSPACE_DIR/data/preprocessed
+python $workspace/whitespace-correction/src/whitespace_correction/preprocess_data.py \
+ --config ${CONFIG?"CONFIG env variable not found"}
