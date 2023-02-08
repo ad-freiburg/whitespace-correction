d7d709446575f739a6e06b850a0516828cd8c750
Sebastian Walter <sebastian.walter98@gmail.com>
2023-02-08 11:55:59 +0100

    update server

--- src/whitespace_correction/api/server.py
+++ src/whitespace_correction/api/server.py
@@ -1,8 +1,10 @@
+import time
 from typing import Dict, Any
 
 from flask import Response, jsonify, request, abort
 
 from text_correction_utils.api.server import TextCorrectionServer, Error
+from text_correction_utils.api.utils import ProgressIterator
 
 from whitespace_correction.api.corrector import WhitespaceCorrector
 
@@ -25,8 +27,20 @@ class WhitespaceCorrectionServer(TextCorrectionServer):
                     if isinstance(cor, Error):
                         return abort(cor.to_response())
                     assert isinstance(cor, WhitespaceCorrector)
-                    corrected = cor.correct_text(json["text"])
+                    start = time.perf_counter()
+                    iter = ProgressIterator(
+                        ((t, None) for t in json["text"]),
+                        size_fn=lambda e: len(e[1].encode("utf8"))
+                    )
+                    corrected = list(cor.correct_iter(iter))
+                    end = time.perf_counter()
+                    kb = iter.total_size / 1000
+                    s = end - start
+                    kbps = kb / s
             except Exception as error:
                 return abort(Response(f"request failed with unexpected error: {error}", status=500))
 
-            return jsonify({"text": corrected})
+            return jsonify({
+                "text": corrected,
+                "runtime": {"kb": kb, "s": s, "kbps": kbps}
+            })
